50f1012408a79ba87940b28de72aa4f03d3b6bd7
Benjamin Hartmann <benjamin@awesomebible.de>
2022-11-24 16:04:28 +0000

    Add blobcat emojis

--- repoData.js
+++ repoData.js
@@ -1,5 +1,5 @@
 //Add Name of your Repo here
-exports.name = 'Example Repo';
+exports.name = 'darkshark\'s custom emojis';
 //Add Name and Type of Image Icon you want to use here (Must be PNG)
 exports.icon = 'icon.png';
 //Add the folder name where all your emotes are saved
@@ -7,6 +7,6 @@ exports.pathName = 'emotes';
 //Add your name here, or remove the line if you don't need your name displayed under the repo's title in our clients
 exports.author = 'Joe';
 //Add a Description here, useful for SEO and Web Crawlers, or sharing to social websites. Remove the line if you don't care about Sharing.
-exports.description = 'Example Description';
+exports.description = 'Nitroless Emojipack';
 //Add Keywords here, useful for SEO and Web Crawlers. Remove the line if you don't care about SEO/Web Crawlers.
 exports.keywords = 'Nitroless, nitroless, iOS, macOS, Android, Windows, Emotes, Discord, discord, Nitro, nitro';
